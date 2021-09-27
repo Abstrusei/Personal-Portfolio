929b392230775cfac7814b0d14c95b9892c0edf0
Sean <smarshallean@gmail.com>
2021-09-28 03:41:57 +1000

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    //"JavaScript is Enabled" Body Class
+    $("body").addClass("js");
+
+    let prevScrollpos = window.pageXOffset;
+    window.addEventListener('scroll', hideOrDisplayNavbar);
+    
+    // Hides menu on scroll
+    function hideOrDisplayNavbar () {
+        let currentScrollpos = window.pageYOffset;
+        if (prevScrollpos > currentScrollpos) {
+            document.getElementById("main-menu").style.top = "0";
+            changeNavbarLogo();
+        } else {
+            document.getElementById("main-menu").style.top = "-130px";
+            changeNavbarLogo();
+        }
+
+        prevScrollpos = currentScrollpos;
+    }
+    
+    // Changes the main logo text in the navbar to "Scroll to Top"
+    let navbarLogo = document.getElementById("main-logo");
+    let defaultNavbarLogo = navbarLogo.innerHTML;
+    function changeNavbarLogo() {
+        if (window.pageYOffset > 600) {
+            console.log("navbar logo should have changed");
+            navbarLogo.innerHTML = "SCROLL TO TOP";
+        }  else {
+            console.log("navbar logo should be the same");
+            navbarLogo.innerHTML = defaultNavbarLogo;
+        }
+    }
+
+});
---
+++ partials/menu.php
@@ -0,0 +1,24 @@
+<?php require_once "config/constants.php"; ?>
+
+<nav id="main-menu">
+    <div class="container flex lg-space-btwn sm-column">
+        <div class="main-logo">
+            <a id="main-logo" href="index.html#intro">
+                <h3>Se<span>an</span></h3>
+            </a>
+        </div>
+        <div id="nav-elements-wrapper">
+            <ul class="flex lg-space-btwn">
+                <li>
+                    <a href="#about-me-section">About Me</a>
+                </li>
+                <li>
+                    <a href="#projects-section">Projects</a>
+                </li>
+                <!-- <li>
+                    <a href="#reflections-section">Reflections</a>
+                </li> -->
+            </ul>
+        </div>
+    </div>
+</nav>
